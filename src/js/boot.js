6f90facd483ffdc42e78fd90330eeaa0d067b0cc
Sam Morrison <sam@sammorrison.net>
2015-08-27 21:28:19 -0400

    loading screen

--- src/js/boot.js
+++ src/js/boot.js
@@ -5,6 +5,7 @@ GameStates.Boot = function (game) {};
 GameStates.Boot.prototype = {
 	init: function () {
 		this.input.maxPointers = 1;
+		game.stage.backgroundColor = '#ffffff';
 	},
 	create: function () {
 		game.state.start('preloader');
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,6 +2,9 @@ GameStates.Preloader = function (game) {};
 
 GameStates.Preloader.prototype = {
 	preload: function () {
+		var loadingText = game.add.text(game.world.centerX, game.world.centerY, 'LOADING', { font: "4em squada one", fill: "#333", align: "center" });
+		loadingText.anchor.set(0.5);
+		
 		this.load.image('background', 'images/cactus.jpg');
 	},
 	create: function () {
