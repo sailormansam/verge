78df0a169f964187bda6a876a85b3a8d7cf4d46e
Sam Morrison <sam@sammorrison.net>
2015-08-28 23:14:48 -0400

    comment

--- src/js/mainmenu.js
+++ src/js/mainmenu.js
@@ -5,7 +5,7 @@ GameStates.MainMenu = function (game) {
 GameStates.MainMenu.prototype = {
 	create: function () {
         var loadingText = game.add.text(game.world.centerX, game.width * 2, 'VERGE', { font: "6em squada one", fill: "#333"});
-		this.background.anchor.setTo(0.5);
-		this.background.scale.set(0.4);
+//		this.background.anchor.setTo(0.5);
+//		this.background.scale.set(0.4);
 	}
 };
