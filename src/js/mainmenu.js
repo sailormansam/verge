9fafcafc607482b3b9dc0768c503aa05c79a2a3f
Sam Morrison <sam@sammorrison.net>
2015-08-29 22:58:27 -0400

    Main menu has play button

--- src/js/mainmenu.js
+++ src/js/mainmenu.js
@@ -5,7 +5,12 @@ GameStates.MainMenu = function (game) {
 GameStates.MainMenu.prototype = {
 	create: function () {
         var loadingText = game.add.text(game.world.centerX, game.width * 2, 'VERGE', { font: "6em squada one", fill: "#333"});
-//		this.background.anchor.setTo(0.5);
-//		this.background.scale.set(0.4);
-	}
+        var playText = game.add.text(game.height * 7, game.width * 2, 'PLAY', { font: "3em squada one", fill: "#333"});
+        playText.inputEnabled = true;
+        playText.events.onInputDown(this.play, this);
+	},
+    play: function() {
+        game.state.start('game');
+    }
+    
 };
