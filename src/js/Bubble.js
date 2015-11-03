d8f0120bd5c9c44a40837c23f394d086ebf3ab48
Sam Morrison <sam@sammorrison.net>
2015-11-02 21:01:33 -0500

    main functions filled out for bubble functionality

--- src/js/Bubble.js
+++ src/js/Bubble.js
@@ -1,7 +1,12 @@
-var Bubble = function (image) {
+var Bubble = function (image, distanceFromPointer) {
 	this.image = image;
 	this.origin = new Phaser.Point(20, 20);
-	this.displayPoint;
+	this.distanceFromPointer = distanceFromPointer;
+	this.origin;
+	this.desiredLocation;
+	this.showing = false;
+	this.velocity = new Phaser.Point(0, 0);
+	this.k = 0.5;
 	
 	// create background
 	var graphics = game.add.graphics(0, 0);
@@ -15,14 +20,49 @@ var Bubble = function (image) {
 
 	// create foreground image
 	
+	// on click
+	
 	game.add.existing(this);
 };
 
 Bubble.prototype = Object.create(Phaser.Sprite.prototype);
 Bubble.prototype.constructor = Bubble;
 
-Bubble.prototype.show = function () {
-	// pop in bubble
+Bubble.prototype.update = function () {
+	if(this.showing) {
+		// set current point
+		var currentPoint = new Phaser.Point(this.x, this.y);
+		
+		// calculate acceleration for x and y
+		var ax = -this.k * Math.abs(desiredLocation.x - currentPoint.x);
+		var ay = -this.k * Math.abs(desiredLocation.y - currentPoint.y);
+		
+		// add to velocity
+		this.velocity.x += ax;
+		this.velocity.y += ay;
+		
+		// add to position
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+		
+		// set scale based on distance to desiredLocation
+		var originDistance = this.origin.distance(this.currentPoint);
+		var currentDistance = this.currentPoint.distance(this.desiredLocation);
+		this.scale.set(currentDistance / originDistance);
+	}
+};
+
+Bubble.prototype.show = function (angle, pointer) {
+	this.showing = true;
+	
+	// set points to control spring
+	this.origin = new Phaser.Point(pointer.x, pointer.y);
+	this.desiredLocation = new Phaser.Point(origin.x + distanceFromPointer * Math.cos(angle),
+											origin.y + distanceFromPointer * Math.sin(angle));
+	
+	// set to origin
+	this.x = origin.x;
+	this.y = origin.y;
 };
 
 Bubble.prototype.hide = function () {
--- src/js/BubbleController.js
+++ src/js/BubbleController.js
@@ -1,22 +1,37 @@
 var BubbleController = function () {
+	this.bubbles;
+	this.angle;
+	
 	this.create();
 };
 
 BubbleController.prototype = {
 	create: function () {
+		// set variables
+		this.bubbles = [];
+		this.angle = 0;
 	},
 	
-	add: function () {
+	add: function (bubble) {
 		// add bubble to bubbles array
+		bubbles.push(bubble);
 		
-		// calculate bubles positions based on bubble array
+		// calculate bubbles positions based on bubble array
+		this.angle = 2 * Math.PI / this.bubbles.length;
+		
 	},
 	
-	show: function () {
+	show: function (pointer) {
 		// show all bubbles and calculate positions
+		for(var i = 0, len = this.bubbles.length; i < len; i++) {
+			bubble.show(i * this.angle, pointer);
+		}
 	},
 	
 	hide: function () {
 		// hide all bubbles
+		this.bubbles.forEach(function (bubble) {
+			bubble.hide();
+		});
 	}
 };
