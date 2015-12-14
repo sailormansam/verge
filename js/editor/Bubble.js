80fd00eb4d4b9216b13066def471d3e5b3daf521
Sam Morrison <sam@sammorrison.net>
2015-12-13 22:52:24 -0500

    Bubbles no longer pop out on editor load

--- js/editor/Bubble.js
+++ js/editor/Bubble.js
@@ -1,6 +1,6 @@
 var Bubble = function (image, distanceFromPointer) {
 	this.image = image;
-	this.origin = new Phaser.Point(20, 20);
+	this.origin;
 	this.distanceFromPointer = distanceFromPointer;
 	this.origin;
 	this.showing = false;
@@ -15,7 +15,7 @@ var Bubble = function (image, distanceFromPointer) {
 	graphics.beginFill(0x999999);
 	graphics.drawCircle(0, 0, 45);
 	
-	Phaser.Sprite.call(this, game, this.origin.x, this.origin.y, graphics.generateTexture());
+	Phaser.Sprite.call(this, game, 0, 0, graphics.generateTexture());
 	
 	graphics.destroy();
 	this.anchor.set(0.5);
@@ -38,50 +38,52 @@ Bubble.prototype = Object.create(Phaser.Sprite.prototype);
 Bubble.prototype.constructor = Bubble;
 
 Bubble.prototype.update = function () {
-	// set current point
-	var currentPoint = new Phaser.Point(this.x, this.y);
+	if(this.origin) {
+		// set current point
+		var currentPoint = new Phaser.Point(this.x, this.y);
 
-	// calculate acceleration for x and y
-	var ax = -this.k * (currentPoint.x - this.desiredLocation.x);
-	var ay = -this.k * (currentPoint.y - this.desiredLocation.y);
+		// calculate acceleration for x and y
+		var ax = -this.k * (currentPoint.x - this.desiredLocation.x);
+		var ay = -this.k * (currentPoint.y - this.desiredLocation.y);
 
-	// add to velocity
-	this.velocity.x += ax * game.time.elapsedMS / 1000;
-	this.velocity.x *= this.dampening;
-	this.velocity.y += ay * game.time.elapsedMS / 1000;
-	this.velocity.y *= this.dampening;
+		// add to velocity
+		this.velocity.x += ax * game.time.elapsedMS / 1000;
+		this.velocity.x *= this.dampening;
+		this.velocity.y += ay * game.time.elapsedMS / 1000;
+		this.velocity.y *= this.dampening;
 
-	// add to position
-	this.x += this.velocity.x * game.time.elapsedMS / 1000;
-	this.y += this.velocity.y * game.time.elapsedMS / 1000;
+		// add to position
+		this.x += this.velocity.x * game.time.elapsedMS / 1000;
+		this.y += this.velocity.y * game.time.elapsedMS / 1000;
 
-	currentPoint.x = this.x;
-	currentPoint.y = this.y;
+		currentPoint.x = this.x;
+		currentPoint.y = this.y;
 
-	// set scale based on distance to desiredLocation
-	var originDistance = this.origin.distance(this.desiredLocation);
-	
-	// invert for hiding
-	if(this.showing) {
-		var currentDistance = this.origin.distance(currentPoint);
+		// set scale based on distance to desiredLocation
+		var originDistance = this.origin.distance(this.desiredLocation);
+
+		// invert for hiding
+		if(this.showing) {
+			var currentDistance = this.origin.distance(currentPoint);
+		}
+		else {
+			var currentDistance = this.desiredLocation.distance(currentPoint);
+		}
+
+		this.scale.set(currentDistance / originDistance);
+
+		var alpha = currentDistance / originDistance;
+
+		if(alpha > 1)
+			alpha = 1;
+
+		if(alpha < 0)
+			alpha = 0;
+
+		this.alpha = alpha;
+
+		// update foreground image
 	}
-	else {
-		var currentDistance = this.desiredLocation.distance(currentPoint);
-	}
-	
-	this.scale.set(currentDistance / originDistance);
-	
-	var alpha = currentDistance / originDistance;
-	
-	if(alpha > 1)
-		alpha = 1;
-	
-	if(alpha < 0)
-		alpha = 0;
-
-	this.alpha = alpha;
-	
-	// update foreground image
 };
 
 Bubble.prototype.show = function (angle, pointer) {
