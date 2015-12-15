5e8415fc36d44065df2b7b4fb85de26262234592
Sam Morrison <sam@sammorrison.net>
2015-12-14 23:53:54 -0500

    Updated gulpfile Fixed #46

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,51 @@ var gulp = require('gulp'),
 	del = require('del');
 
 gulp.task('default', ['del'], function () {
-	gulp.start('minify-js', 'minify-css', 'imagemin', 'html-replace', 'copy', 'copy-fonts');
+	gulp.start('minify-js-game', 'minify-js-editor', 'minify-js-lib', 'html-replace-index', 'html-replace-editor', 'minify-css', 'imagemin', 'copy', 'copy-fonts');
 });
 
 gulp.task('del', function (cb) {
 	del(['dist/**', '!dist'], cb);
 });
 
-gulp.task('html-replace', function () {
-	return gulp.src('src/index.html')
-		.pipe(htmlreplace({ js: 'js/main.min.js', css: 'css/main.min.css' }))
+gulp.task('html-replace-index', function () {
+	return gulp.src('index.html')
+		.pipe(htmlreplace({ js: ['js/game.min.js', 'js/lib.min.js'], css: 'css/main.min.css' }))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-js', function () {
-	return gulp.src('src/js/*.js')
-		.pipe(concat('main.js'))
+gulp.task('html-replace-editor', function () {
+	return gulp.src('editor.html')
+		.pipe(htmlreplace({ js: ['js/editor.min.js', 'js/lib.min.js'], css: 'css/main.min.css' }))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js-game', function () {
+	return gulp.src('js/game/*.js')
+		.pipe(concat('game.js'))
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-js-editor', function () {
+	return gulp.src('js/editor/*.js')
+		.pipe(concat('editor.js'))
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-js-lib', function () {
+	return gulp.src('js/lib/*.js')
+		.pipe(concat('lib.js'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('minify-css', function () {
-	return gulp.src('src/css/*.css')
+	return gulp.src('css/*.css')
 		.pipe(concat('main.css'))
 		.pipe(minifycss())
 		.pipe(rename({suffix: '.min'}))
@@ -39,17 +61,17 @@ gulp.task('minify-css', function () {
 });
 
 gulp.task('imagemin', function () {
-	return gulp.src('src/images/*')
+	return gulp.src('images/*')
 		.pipe(imagemin({progressive: true}))
 		.pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('copy', function () {
-	return gulp.src('src/phaser.min.js')
+	return gulp.src('phaser.min.js')
 		.pipe(copy('dist', { prefix: 1 }));
 });
 
 gulp.task('copy-fonts', function() {
-   return gulp.src('src/fonts/**')
-    .pipe(copy('dist', { prefix: 1 }));
+	return gulp.src('fonts/**')
+		.pipe(copy('dist/fonts', { prefix: 1 }));
 });
