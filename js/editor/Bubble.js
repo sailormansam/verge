25d1969b9efe6dd32f1c06882f00c35f6dbd9932
Sam Morrison <sam@sammorrison.net>
2015-12-29 23:03:22 -0500

    fixed bubble initial jump

--- js/editor/Bubble.js
+++ js/editor/Bubble.js
@@ -10,26 +10,28 @@ var Bubble = function (parent, action, distanceFromPointer) {
 	this.action = action;
 	this.active = false;
 	
+	
 	// call group which this extends
 	Phaser.Group.call(this, game);
 	game.add.existing(this);
+	
+	// set origin to actual loctaion
+	this.x = this.origin.x;
+	this.y = this.origin.y;
 
 	// create foreground image
-	this.foreground = game.add.existing(action.sprite);
 	this.background = game.add.sprite(0, 0, 'bubble');
+	this.foreground = game.add.existing(action.sprite);
 	
 	// add background and foreground
-	this.add(this.foreground);
 	this.add(this.background);
-	
-	// make sure foreground is on top of background
-	this.foreground.bringToTop();
+	this.add(this.foreground);
 	
 	this.background.anchor.set(0.5);
 	this.foreground.anchor.set(0.5);
 	this.foreground.scale.set(0.5);
 	
-	// on hover
+	// on click
 	this.background.inputEnabled = true;
 	this.background.input.useHandCursor = true;
 	this.background.events.onInputDown.add(this.click, this);
@@ -44,65 +46,64 @@ Bubble.prototype = Object.create(Phaser.Group.prototype);
 Bubble.prototype.constructor = Bubble;
 
 Bubble.prototype.update = function () {
-	if(this.origin) {
-		// set current point
-		var currentPoint = new Phaser.Point(this.x, this.y);
+	// set current point
+	var currentPoint = new Phaser.Point(this.x, this.y);
 
-		// calculate acceleration for x and y
-		var ax = -this.k * (currentPoint.x - this.desiredLocation.x);
-		var ay = -this.k * (currentPoint.y - this.desiredLocation.y);
+	// calculate acceleration for x and y
+	var ax = -this.k * (currentPoint.x - this.desiredLocation.x);
+	var ay = -this.k * (currentPoint.y - this.desiredLocation.y);
 
-		// add to velocity
-		this.velocity.x += ax * game.time.elapsedMS / 1000;
-		this.velocity.x *= this.dampening;
-		this.velocity.y += ay * game.time.elapsedMS / 1000;
-		this.velocity.y *= this.dampening;
+	// add to velocity
+	this.velocity.x += ax * game.time.elapsedMS / 1000;
+	this.velocity.x *= this.dampening;
+	this.velocity.y += ay * game.time.elapsedMS / 1000;
+	this.velocity.y *= this.dampening;
 
-		// add to position
-		this.x += (this.velocity.x * game.time.elapsedMS / 1000);
-		this.y += (this.velocity.y * game.time.elapsedMS / 1000);
-		
-		currentPoint.x = this.x;
-		currentPoint.y = this.y;
-		// set scale based on distance to desiredLocation
-		var originDistance = this.origin.distance(this.desiredLocation);
+	// add to position
+	this.x += (this.velocity.x * game.time.elapsedMS / 1000);
+	this.y += (this.velocity.y * game.time.elapsedMS / 1000);
 
-//		// invert for hiding
-		if(!this.active) {
-			if(this.showing) {
-				var currentDistance = this.origin.distance(currentPoint);
-			}
-			else {
-				var currentDistance = this.desiredLocation.distance(currentPoint);
-			}
-	
-			// cap scale
-			var scale = currentDistance / originDistance;
-			if(scale > 1.3) {
-				scale = 1.3
-			}
-			
-			if (scale < 0) {
-				scale = 0;
-			}
-			
-			this.scale.set(scale);
-	
-			var alpha = currentDistance / originDistance;
-	
-			if(alpha > 1)
-				alpha = 1;
-	
-			if(alpha < .1) {
-				alpha = 0;
-				this.background.visible = false
-			}
-			else {
-				this.background.visible = true;
-			}
-	
-			this.alpha = alpha;
+	currentPoint.x = this.x;
+	currentPoint.y = this.y;
+
+	// set scale based on distance to desiredLocation
+	var originDistance = this.origin.distance(this.desiredLocation);
+
+	// invert for hiding
+	if(!this.active) {
+		if(this.showing) {
+			var currentDistance = this.origin.distance(currentPoint);
 		}
+		else {
+			var currentDistance = this.desiredLocation.distance(currentPoint);
+		}
+
+		// cap scale
+		var scale = currentDistance / originDistance;
+		if(scale > 1.3) {
+			scale = 1.3
+		}
+
+		if (scale < 0) {
+			scale = 0;
+		}
+
+		this.scale.set(scale);
+
+		var alpha = currentDistance / originDistance;
+
+		if(alpha > 1)
+			alpha = 1;
+
+		if(alpha < .1) {
+			alpha = 0;
+			this.background.visible = false
+		}
+		else {
+			this.background.visible = true;
+		}
+
+		this.alpha = alpha;
 	}
 };
 
