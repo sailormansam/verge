23a7a84acb16d8be125cc36a91689f2612135dbe
Sam Morrison <sam@sammorrison.net>
2016-02-08 22:28:17 -0500

    rotation arm doesn't point right to pointer now

--- js/editor/RotationOverlay.js
+++ js/editor/RotationOverlay.js
@@ -3,6 +3,9 @@ var RotationOverlay = function (parent) {
 	this.UI = parent;
     this.rotationGraphics;
     this.rot;
+    this.grab = false;
+    this.grabAngle = 0;
+    this.radians = 0;
     
 	Overlay.call(this, parent);
     
@@ -27,51 +30,62 @@ RotationOverlay.prototype = Object.create(Overlay.prototype);
 RotationOverlay.prototype.constructor = RotationOverlay;
 
 RotationOverlay.prototype.update = function () {
-    // clear graphics
-    this.rotationGraphics.clear();
-    
-    // draw small circle for appearance
-    this.rotationGraphics.beginFill(0x202529);
-    this.rotationGraphics.lineStyle(0, 0x202529, 1);
-    this.rotationGraphics.drawCircle(game.width / 2, game.height / 2, 5);
-    this.rotationGraphics.endFill();
-    
-    // set line style
-    this.rotationGraphics.lineStyle(5, 0x202529, 1);
-    
-    // draw base
-    this.rotationGraphics.moveTo(game.width / 2, game.height / 2);
-    this.rotationGraphics.lineTo(game.width / 2 + 50, game.height / 2);
-    
-    // draw angle
-    this.rotationGraphics.moveTo(game.width / 2, game.height / 2);
     var centerPoint = new Phaser.Point(game.width / 2, game.height / 2);
-    var mousePoint = new Phaser.Point(game.input.x, game.input.y);
-    var vector = Phaser.Point.subtract(centerPoint, mousePoint);
-    var vectorLength = Phaser.Point.distance(centerPoint, mousePoint);
-    var normalized = Phaser.Point.divide(vector, new Phaser.Point(vectorLength, vectorLength));
-    var multiplied = Phaser.Point.multiply(normalized, new Phaser.Point(50, 50));
-    var newCenterPoint = Phaser.Point.subtract(centerPoint, multiplied);
-    this.rotationGraphics.lineTo(newCenterPoint.x, newCenterPoint.y);
     
-    // update angle text
-    var radians = Math.atan2(game.input.y - centerPoint.y, game.input.x - centerPoint.x)
-    var angle = radians * (180 / Math.PI);
-    this.rotationText.text = angle.toFixed(0) + '°';
-    
-    // draw arc
-    this.rotationGraphics.lineStyle(5, 0xf1f1f1);
-    
-    var antiClockwise = true;
-    
-    // arc rotation direction
-    if (radians > 0) {
-        antiClockwise = false;
+    // Only rotate when dragging
+    if(game.input.activePointer.leftButton.isDown) {
+         // get initial mouse position relative to arm
+        if(!this.grab) {
+            this.grabAngle = this.radians - Math.atan2(game.input.y - centerPoint.y, game.input.x - centerPoint.x);
+            this.grab = true;
+        }
+        
+        // clear graphics
+        this.rotationGraphics.clear();
+
+        // draw small circle for appearance
+        this.rotationGraphics.beginFill(0x202529);
+        this.rotationGraphics.lineStyle(0, 0x202529, 1);
+        this.rotationGraphics.drawCircle(game.width / 2, game.height / 2, 5);
+        this.rotationGraphics.endFill();
+
+        // set line style
+        this.rotationGraphics.lineStyle(5, 0x202529, 1);
+
+        // draw base
+        this.rotationGraphics.moveTo(game.width / 2, game.height / 2);
+        this.rotationGraphics.lineTo(game.width / 2 + 50, game.height / 2);
+
+        // draw angle
+        this.rotationGraphics.moveTo(game.width / 2, game.height / 2);
+        var newCenterPoint = new Phaser.Point();
+        newCenterPoint.x = Math.cos(this.radians) * 50 + centerPoint.x;
+        newCenterPoint.y = Math.sin(this.radians) * 50 + centerPoint.y;
+        this.rotationGraphics.lineTo(newCenterPoint.x, newCenterPoint.y);
+        
+        // update angle text
+        this.radians = Math.atan2(game.input.y - centerPoint.y, game.input.x - centerPoint.x) + this.grabAngle;
+        var angle = this.radians * (180 / Math.PI);
+        this.rotationText.text = angle.toFixed(0) + '°';
+
+        // draw arc
+        this.rotationGraphics.lineStyle(5, 0xf1f1f1);
+
+        var antiClockwise = true;
+
+        // arc rotation direction
+        if (this.radians > 0) {
+            antiClockwise = false;
+        }
+
+        // save angle
+        this.rot = angle.toFixed(0);
+
+        // display arc
+        this.rotationGraphics.arc(centerPoint.x, centerPoint.y, 100, 0, this.radians, antiClockwise);
     }
-    
-    // save angle
-    this.rot = angle.toFixed(0);
-    
-    // display arc
-    this.rotationGraphics.arc(centerPoint.x, centerPoint.y, 100, 0, radians, antiClockwise);
+    else {
+        this.grab = false;
+        this.grabAngle = 0;
+    }
 };
